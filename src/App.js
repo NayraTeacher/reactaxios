446c0e9e5425215ff2181d6c6937a91e71bf0e14
Nayra Deniz <nayra.deniz@u-tad.com>
2021-11-24 12:20:46 +0100

    primeros componentes y axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Table from './components/Table';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cabecera: ['Album', 'Identificador', 'Titulo', 'Imagen'],
+      datos: []
+    }
+
+  }
+
+  // Uso del ciclo de vida de los componentes: https://reactjs.org/docs/state-and-lifecycle.html
+  componentDidMount() {
+    //Uso de axios con react: https://www.freecodecamp.org/news/how-to-use-axios-with-react/
+    const url = 'https://jsonplaceholder.typicode.com/photos';
+    axios.get(url).then((response) => {
+        this.setState({datos: response.data});
+    });
 }
 
+  render() {
+    return (
+      <div className="App">
+        <Header ejercicio="Axios"></Header>
+        <Table cabeceras={this.state.cabecera} filas={this.state.datos}></Table>
+      </div>
+    );
+  }
+}
 export default App;
